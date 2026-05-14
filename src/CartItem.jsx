9af8a19bbe33cda825307bc2c03145011f8953d9
Sanele Zwane <sanelezwa@gmail.com>
2026-05-14 18:38:01 +0200

    Updated

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,62 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+ return cart
+    .reduce((total, item) => {
+
+      const itemTotal =
+        parseFloat(item.cost.substring(1)) *
+        item.quantity;
+
+      return total + itemTotal;
+
+    }, 0)
+    .toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
-  };
 
+      onContinueShopping(e);
+
+  };
 
 
   const handleIncrement = (item) => {
+    
+    dispatch(updateQuantity({
+    name: item.name,
+    amount: item.quantity + 1,
+  }));
   };
 
   const handleDecrement = (item) => {
+
+    if (item.quantity > 1) {
+
+    dispatch(updateQuantity({
+      name: item.name,
+      amount: item.quantity - 1,
+    }));
+
+  } else {
+
+    dispatch(removeItem(item.name));
+
+  }
    
   };
 
   const handleRemove = (item) => {
+
+      dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  return (
+    parseFloat(item.cost.substring(1)) *
+    item.quantity
+  ).toFixed(2);
   };
 
   return (
